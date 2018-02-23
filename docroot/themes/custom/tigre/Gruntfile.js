f58c8a2d823df23dfdcc4ae2ae93bcffa33e7d3e
Lucas Guszak <lucas.guszak@gmail.com>
2018-02-23 01:37:12 -0300

    grunt and sass

---
+++ docroot/themes/custom/tigre/Gruntfile.js
@@ -0,0 +1,60 @@
+// Load Grunt
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // Tasks
+        sass: { // Begin Sass Plugin
+            dist: {
+                options: {
+                    sourcemap: 'none'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/sass',
+                    src: ['**/*.scss'],
+                    dest: 'css',
+                    ext: '.css'
+                }]
+            }
+        },
+        postcss: { // Begin Post CSS Plugin
+            options: {
+              map: false,
+              processors: [
+            require('autoprefixer')({
+                  browsers: ['last 2 versions']
+                })
+          ]
+            },
+            dist: {
+              src: 'css/style.css'
+            }
+          },
+          cssmin: { // Begin CSS Minify Plugin
+            target: {
+              files: [{
+                expand: true,
+                cwd: 'css',
+                src: ['*.css', '!*.min.css'],
+                dest: 'css',
+                ext: '.min.css'
+          }]
+            }
+          },
+        watch: { // Compile everything into one task with Watch Plugin
+            css: {
+                files: 'assets/sass/*.scss',
+                tasks: ['sass', 'postcss', 'cssmin']
+            }
+        }
+    });
+    // Load Grunt plugins
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Register Grunt tasks
+    grunt.registerTask('default', ['watch']);
+};
