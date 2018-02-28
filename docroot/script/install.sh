cfda61b0b9136434fa3325344f3f8551ff02c5a4
Júlio Kamizato <julio.kamizato@mmda.com.br>
2018-02-28 01:32:11 -0300

    UUid pt br

--- docroot/script/install.sh
+++ docroot/script/install.sh
@@ -5,5 +5,6 @@
 drush si tigre -y
 drush config-set "system.site" uuid "65e853d3-e79d-4b6c-9eb0-a308e5beb4b8" -y
 drush config-set "language.entity.en" "uuid" "65e853d3-e79d-4b6c-9eb0-a308e5beb4b8" -y
+drush config-set "language.entity.pt-br" "uuid" "65e853d3-e79d-4b6c-9eb0-a308e5beb4b8" -y
 drush cim -y --quiet
 drush cr
