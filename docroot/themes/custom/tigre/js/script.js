b8d38b8a0cbd76dd8c87af3a0a0c2271aaecb8c9
Lucas Guszak <lucas.guszak@gmail.com>
2018-03-05 00:03:28 -0300

    Catalogos front, login, contato and smtp module

--- docroot/themes/custom/tigre/js/script.js
+++ docroot/themes/custom/tigre/js/script.js
@@ -13,15 +13,129 @@
                         "margin-top": "-39px",
                         "box-shadow": "0 1px 0 rgba(12,13,14,0.1), 0 1px 3px rgba(12,13,14,0.1), 0 4px 20px rgba(12,13,14,0.035), 0 1px 1px rgba(12,13,14,0.025)"
                         });
+                    $(".menu--main li a").css({
+                            'color': '#000000'
+                    });
+                    // $("#navbar-collapse .logo img").css({
+                    //         'background': 'url(themes/custom/tigre/assets/images/logo-azul.png) no-repeat'
+                    //     });
+                    //$("#navbar-collapse .logo img")[0].src = 'themes/custom/tigre/assets/images/logo-azul.png';
                     } else {
                     $('#navbar-collapse').css({
                         'background-color': 'transparent',
                         "margin-top": "0",
                         "box-shadow": "none"
                         });
+                        $(".menu--main li a").css({
+                            'color': '#ffffff'
+                    });
                     }
                 });
             //}
+
+            // $('#tree3 li').find('a').each(function () {
+            //     if (document.location.pathname == $(this).attr('href')) {
+            //         $(this).parents().addClass("open");
+            //         $(this).addClass("open");
+            //         // add class as you need ul or li or a 
+            //     }
+            // });
+
+           
+            
+            //Initialization of treeviews
+            
+            // $('#tree1').treed();
+            
+            // $('#tree2').treed({openedClass:'glyphicon-folder-open', closedClass:'glyphicon-folder-close'});
+            $(document).ready(function(){
+
+                // $(function () {
+                //     $( "#tree3 .selected" ).parents('#tree3 .branch').map(function() {
+                //     console.log($(this));
+                //     var icon = $(this).children('i:first');
+
+                //     icon.toggleClass('glyphicon-folder-open glyphicon-folder-close');
+
+                //     $(this).children().children().toggle();
+                //     })
+
+                // });
+
+                
+
+                $.fn.extend({
+                    treed: function (o) {
+                      
+                      var openedClass = 'glyphicon-minus-sign';
+                      var closedClass = 'glyphicon-plus-sign';
+                      
+                      if (typeof o != 'undefined'){
+                        if (typeof o.openedClass != 'undefined'){
+                        openedClass = o.openedClass;
+                        }
+                        if (typeof o.closedClass != 'undefined'){
+                        closedClass = o.closedClass;
+                        }
+                      };
+                      
+                        //initialize each of the top levels
+                        var tree = $(this);
+                        tree.addClass("tree");
+                        tree.find('li').has("ul").each(function () {
+                            var branch = $(this); //li with children ul
+                            branch.prepend("<i class='indicator glyphicon " + closedClass + "'></i>");
+                            branch.addClass('branch');
+                            branch.on('click', function (e) {
+                                if (this == e.target) {
+                                    var icon = $(this).children('i:first');
+                                    icon.toggleClass(openedClass + " " + closedClass);
+                                    $(this).children().children().toggle();
+                                }
+                            })
+                            branch.children().children().toggle();
+                        });
+                        //fire event from the dynamically added icon
+                      tree.find('.branch .indicator').each(function(){
+                        $(this).on('click', function () {
+                            $(this).closest('li').click();
+                        });
+                      });
+                        //fire event to open branch if the li contains an anchor instead of text
+                        // tree.find('.branch>a').each(function () {
+                        //     $(this).on('click', function (e) {
+                        //         $(this).closest('li').click();
+                        //         e.preventDefault();
+                        //     });
+                        // });
+                        //fire event to open branch if the li contains a button instead of text
+                        tree.find('.branch>button').each(function () {
+                            $(this).on('click', function (e) {
+                                $(this).closest('li').click();
+                                e.preventDefault();
+                            });
+                        });
+                        
+                    }
+                });
+
+                $('#tree3', context).once('myCustomBehavior').each(function () {
+                    $(this).treed({openedClass:'glyphicon-chevron-right', closedClass:'glyphicon-chevron-down'});
+
+                    $('#tree3 li').find('a').each(function () {
+
+                        if (document.location.pathname == $(this).attr('href')) {
+                            
+                            $(this).parents('#tree3 .branch').map(function() {
+                                var icon = $(this).children('i:first');
+                                icon.toggleClass('glyphicon-chevron-right glyphicon-chevron-down');
+                                $(this).children().children().toggle();
+                            })
+                        }
+                    });
+
+                });
+            });
         }
     };
 })(jQuery, Drupal);
