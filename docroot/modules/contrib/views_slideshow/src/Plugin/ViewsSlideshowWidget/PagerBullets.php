7686339ab27feda03a6445365cbed29bb94dbf3f
Lucas Guszak <lucas.guszak@gmail.com>
2018-03-06 19:26:13 -0300

    Listagem de produtos

--- docroot/modules/contrib/views_slideshow/src/Plugin/ViewsSlideshowWidget/PagerBullets.php
+++ docroot/modules/contrib/views_slideshow/src/Plugin/ViewsSlideshowWidget/PagerBullets.php
@@ -14,5 +14,39 @@ use Drupal\views_slideshow\ViewsSlideshowWidgetBase;
  *   label = @Translation("Bullets"),
  * )
  */
-class PagerBullets extends ViewsSlideshowWidgetBase {
+class PagerBullets extends ViewsSlideshowWidgetBase
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function defaultConfiguration()
+    {
+        return [
+            'views_slideshow_pager_bullets_hover' => ['default' => 0],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildConfigurationForm(array $form, FormStateInterface $form_state)
+    {
+        // Add field to see if they would like to activate slide and pause on pager
+        // hover.
+        $form['views_slideshow_pager_bullets_hover'] = [
+            '#type' => 'checkbox',
+            '#title' => $this->t('Activate Slide on Pager Hover'),
+            '#default_value' => $this->getConfiguration()['views_slideshow_pager_bullets_hover'],
+            '#description' => $this->t('Should the slide be activated and hovering over a pager bullet item.'),
+            '#states' => [
+                'visible' => [
+                    ':input[name="' . $this->getConfiguration()['dependency'] . '[enable]"]' => ['checked' => true],
+                    ':input[name="' . $this->getConfiguration()['dependency'] . '[type]"]' => ['value' => 'views_slideshow_pager_bullets'],
+                ],
+            ],
+        ];
+        return $form;
+    }
+
 }
